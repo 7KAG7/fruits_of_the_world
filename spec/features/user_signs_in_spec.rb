3fbec66ed58f3204e25efd159923e1b5376a1b0c
Sam <homeboy@Sams-MacBook-Pro.local>
2019-10-17 15:46:15 -0400

    edited spec file and factory bot

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -11,6 +11,7 @@ feature 'user signs in', %Q{
     visit new_user_session_path
 
     fill_in 'Email', with: user.email
+    fill_in 'Name', with: user.name
     fill_in 'Password', with: user.password
 
     click_button 'Log in'
