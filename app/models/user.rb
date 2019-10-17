abafafde1e6de4811d0ec86a5902123d72a00b2f
Sam <homeboy@Sams-MacBook-Pro.local>
2019-10-17 15:33:20 -0400

    corrected various merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,6 @@
 class User < ApplicationRecord
-  # mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :recoverable, :rememberable, :validatable
-  has_many :fruits, through: :reviews
-  has_many :reviews
-=======
-         :recoverable, :rememberable, :trackable, :validatable
-
-         attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
-
-  validates_presence_of   :avatar
-  validates_integrity_of  :avatar
-  validates_processing_of :avatar
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   root 'fruits#index'
   devise_for :users
 
-<<<<<<< HEAD
   resources :fruits, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
   end
 
   resources :users, only: [:show]
-  
-=======
   resources :fruits, only: [:index, :show]
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
