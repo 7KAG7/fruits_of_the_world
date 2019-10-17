4849da2788190944d92c30c4429afc4d5e82126e
7KAG7 <guernsey.keith@gmail.com>
2019-10-17 15:19:00 -0400

    corrected example test file

--- app/javascript/react/components/example.test.js
+++ app/javascript/react/components/example.test.js
@@ -1,11 +1,4 @@
 describe('example test', () => {
-<<<<<<< HEAD
  it('should pass', () => {
    expect(true).toBe(true);
  });
-=======
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
->>>>>>> e47158b27755886b229a20a92fe4ab7c25b6d8e7
-});
