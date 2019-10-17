4786b1aea12f7640f13b0ea3ab14aab9b23e1b57
Ken Charette <charettekenneth@gmail.com>
2019-10-17 16:24:26 -0400

    correcting merge errors

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -5,10 +5,7 @@ class FruitsController < ApplicationController
 
   def show
     @fruit = Fruit.find(params[:id])
-<<<<<<< HEAD
     @review = Review.new
     @reviews = @fruit.reviews
-=======
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_10_16_213124) do
-=======
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_10_17_175055) do
-=======
-ActiveRecord::Schema.define(version: 2019_10_16_142630) do
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
->>>>>>> ba31383b0054ca5c9330a38f5c4dfb5b3a412cb4
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_142630) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "fruit_id"
@@ -43,8 +34,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_142630) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-=======
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,17 +54,13 @@ ActiveRecord::Schema.define(version: 2019_10_16_142630) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.string "role", default: "member", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-=======
     t.string "name", null: false
     t.string "location"
     t.string "favorite_fruit"
->>>>>>> ba31383b0054ca5c9330a38f5c4dfb5b3a412cb4
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
-
 end
