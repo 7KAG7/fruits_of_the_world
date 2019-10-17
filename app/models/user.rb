56a37420e800a9c3451bab74d4f24bc1c5a7a66c
Ken Charette <charettekenneth@gmail.com>
2019-10-17 16:18:05 -0400

    Updated the user.rb file model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,6 @@
-class User < ApplicationRecord
-  # mount_uploader :avatar, AvatarUploader
+  class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  has_many :fruits, through: :reviews
-  has_many :reviews
-         :recoverable, :rememberable, :trackable, :validatable
-
-         attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
-
-  validates_presence_of   :avatar
-  validates_integrity_of  :avatar
-  validates_processing_of :avatar
+         :recoverable, :rememberable, :validatable
 end
