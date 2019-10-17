60dc92d6f06a027360815f5ad1766e67e4530aa0
Ken Charette <charettekenneth@gmail.com>
2019-10-17 16:13:25 -0400

    Fixed errors in routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,14 @@
 Rails.application.routes.draw do
   root 'fruits#index'
   devise_for :users
-<<<<<<< HEAD
-  
-=======
 
-<<<<<<< HEAD
->>>>>>> 0c91857547d777324f8c32b8fc1f4842688a606a
   resources :fruits, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
   end
 
   resources :users, only: [:show]
-<<<<<<< HEAD
+
   resources :fruits, only: [:index, :show]
-=======
-  
-=======
-  resources :fruits, only: [:index, :show]
->>>>>>> 1b08bdae90a07595cace82381c555062ff1df098
->>>>>>> 0c91857547d777324f8c32b8fc1f4842688a606a
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
