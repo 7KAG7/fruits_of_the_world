d3f5b4ab0913ad74b1dabbb13f12ed474310fa55
7KAG7 <guernsey.keith@gmail.com>
2019-10-18 11:37:34 -0400

    aligned users migrations

--- db/migrate/20191017175055_update_users_table.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUsersTable < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :name, :string, null: false
-    add_column :users, :location, :string
-    add_column :users, :favorite_fruit, :string 
-  end
-end
