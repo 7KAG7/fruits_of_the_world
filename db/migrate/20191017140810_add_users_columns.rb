cbc6aec70782b025cb481321eaf3e55d436e8298
7KAG7 <guernsey.keith@gmail.com>
2019-10-18 11:34:40 -0400

    removed avatar from users migration

--- db/migrate/20191017140810_add_users_columns.rb
+++ db/migrate/20191017140810_add_users_columns.rb
@@ -2,6 +2,5 @@ class AddUsersColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :location, :string
     add_column :users, :favorite_fruit, :string
-    add_column :users, :avatar, :string
   end
 end
