3067bc8e52fba43eb7092f715b81f119398f9a98
Ken Charette <charettekenneth@gmail.com>
2019-10-22 14:26:44 -0400

    Added carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if !Rails.env.test?
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["AKIAZSAVKLAGHTY4QTN2"],
-      aws_secret_access_key: ENV["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"]
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     }
     if Rails.env.production?
       config.fog_directory  = ENV["fotwproduction"]
