ca4e36a17d7e377303589fe43e7fa24ada015d0d
Ken Charette <charettekenneth@gmail.com>
2019-10-24 16:19:13 -0400

    Didn't save

--- app/javascript/react/components/ReviewFormContainer.js
+++ app/javascript/react/components/ReviewFormContainer.js
@@ -23,7 +23,6 @@ const ReviewFormContainer = props => {
 
   return(
     <center>
-<<<<<<< HEAD
       <form onSubmit={formSubmit} className="new-review-form">
           <label htmlFor="rating">
             <h2 className="rating-title">Fruit Rating</h2>
@@ -54,31 +53,6 @@ const ReviewFormContainer = props => {
           </label>
         <div className="button-group">
           <input className="button" type="submit" value="Submit" />
-=======
-      <form onSubmit={props.handleFruitForm} className="new-fruit-form callout">
-        <label htmlFor="rating">
-          Fruit Rating
-          <select name="rating" value={newReview.rating} onChange={props.handleFruitFieldChange}>
-              <option name=""></option>
-              <option name="1">1</option>
-              <option name="2">2</option>
-              <option name="3">3</option>
-              <option name="4">4</option>
-              <option name="5">5</option>
-          </select>
-        </label>
-        <p> </p>
-        <label htmlFor="body">Comment
-          <textarea
-            name="body"
-            id="body"
-            value={newReview.body}
-            onChange={handleFruitFieldChange}
-          />
-        </label>
-        <div className="button-group">
-          <input onChange={handleFruitFieldChange} className="button" type="submit" value="Submit" />
->>>>>>> fruit_images
         </div>
       </form>
     </center>
