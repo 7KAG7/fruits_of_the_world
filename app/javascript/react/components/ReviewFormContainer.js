f27bab8f7f03771000dc088c79f51fb76e20afd8
Ken Charette <charettekenneth@gmail.com>
2019-10-24 16:13:54 -0400

    Review form changes

--- app/javascript/react/components/ReviewFormContainer.js
+++ app/javascript/react/components/ReviewFormContainer.js
@@ -21,42 +21,41 @@ const ReviewFormContainer = props => {
     props.postNewReview(formPayLoad)
   }
 
-
   return(
-    <form onSubmit={formSubmit} className="new-review-form">
-      <ul>
-        <label htmlFor="rating">
-          Fruit Rating
-          <select
-            name="rating"
-            id="rating"
-            type="select"
-            value={newReview.rating}
-            onChange={addNewReview}
-          >
-              <option name=""></option>
-              <option name="1">1</option>
-              <option name="2">2</option>
-              <option name="3">3</option>
-              <option name="4">4</option>
-              <option name="5">5</option>
-          </select>
-        </label>
-        <label htmlFor="body">
-          Fruit Body
-          <textarea
-            name="body"
-            id="body"
-            type="text"
-            value={newReview.body}
-            onChange={addNewReview}
-          />
-        </label>
-      </ul>
-      <div className="button-group">
-        <input className="button" type="submit" value="Submit" />
-      </div>
-    </form>
+    <center>
+      <form onSubmit={formSubmit} className="new-review-form">
+          <label htmlFor="rating">
+            <h2 className="rating-title">Fruit Rating</h2>
+            <select
+              name="rating"
+              id="rating"
+              type="select"
+              value={newReview.rating}
+              onChange={addNewReview}
+            >
+                <option name=""></option>
+                <option name="1">1</option>
+                <option name="2">2</option>
+                <option name="3">3</option>
+                <option name="4">4</option>
+                <option name="5">5</option>
+            </select>
+          </label>
+          <label htmlFor="body">
+            <h2 className="body-box">Fruit Comment</h2>
+            <textarea
+              name="body"
+              id="body"
+              type="text"
+              value={newReview.body}
+              onChange={addNewReview}
+            />
+          </label>
+        <div className="button-group">
+          <input className="button" type="submit" value="Submit" />
+        </div>
+      </form>
+    </center>
   )
 }
 
